47c602ed433157be2a304a697bc488d7a4e7b7fa
Ehab H <alwher@live.com>
2025-10-23 15:02:39 +0000

    new

--- models/TestFunc/trans_customers.sql
+++
@@ -1,3 +0,0 @@
-{{ config(materlized='ephemeral')}}
-
-select distinct first_name from {{ ref('stg_customers')}} 
--- models/example/my_second_dbt_model.sql
+++
@@ -1,7 +0,0 @@
-{{ config(materlized='ephemeral')}}
-
--- Use the `ref` function to select from other models
-
-select *
-from {{ ref('my_first_dbt_model') }}
-where id = 1
